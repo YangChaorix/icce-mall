236e1e701629193b16749073072794f8ebce90e3
chao.yang <1025411125@qq.com>
2024-10-02 14:26:46 +0800

    项目初始化

---
+++ ElectronicMallVue/src/App.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Description: 
+ * @Author: 
+ * @Date: 2024-03-26 15:27:05
+-->
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+@import "./resource/css/icon.css";
+</style>
+
+
